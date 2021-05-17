a2eec2029e5072e58e37cdf57bac268fefe1d82c
negin-titanium <neginshahany@gmail.com>
2021-05-17 17:46:52 +0430

    add constructor and state to Game

--- src/components/game.js
+++ src/components/game.js
@@ -1,14 +1,28 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import Board from './board'
 import './components.css'
 
 class Game extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null),
+                    lastMove: Array(2).fill(null)
+                }
+            ],
+            stepNumber: 0,
+            xIsNext: true,
+            sortDesc: false
+        };
+    }
+    
     render() {
         return (
             <div className="game">
                 Tic Tac Toe
-            <Board/>
+            <Board />
             </div>
         );
     }
