35ca3828457aacd3d0b24be987666b22d5d4ff41
negin-titanium <neginshahany@gmail.com>
2021-05-20 11:14:16 +0430

    add material ui to my buttons

--- src/components/square.js
+++ src/components/square.js
@@ -1,13 +1,14 @@
 import React,{ Component} from 'react'
 import './components.css'
+import Button from '@material-ui/core/Button';
 
 class Square extends Component {
     render() { 
         return (
-            <button className={"square" + (this.props.isWinner ? " winner" : "")}
+            <Button className={"square" + (this.props.isWinner ? " winner" : "")}
                 onClick={this.props.onClick}>
                 {this.props.value} 
-            </button>
+            </Button>
         );
     }
 
