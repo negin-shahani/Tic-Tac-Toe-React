4a0461ce78bb1d9922268c04e951001f9950dcf0
negin-titanium <neginshahany@gmail.com>
2021-05-20 16:32:56 +0430

    edit the footer

--- src/components/game.js
+++ src/components/game.js
@@ -6,30 +6,13 @@ import Sort from './sort'
 import { Alert, AlertTitle } from '@material-ui/lab';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-
+import GitHubIcon from '@material-ui/icons/GitHub';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-    },
-    main: {
-        marginTop: theme.spacing(8),
-        marginBottom: theme.spacing(2),
-    },
-    footer: {
-        padding: theme.spacing(3, 2),
-        marginTop: 'auto',
-        backgroundColor:
-            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-    },
-}));
 
 class Game extends Component {
     constructor(props) {
@@ -188,7 +171,7 @@ class Game extends Component {
                 </div>
                 <footer className="footer">
                     <Container maxWidth="sm">
-                        <Typography variant="body1">My sticky footer can be found here.</Typography>
+                        <Typography variant="body1">All Rights Reserved</Typography>
                         <Copyright />
                     </Container>
                 </footer>
@@ -226,11 +209,12 @@ function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-        </Link>{' '}
+            {' '}
             {new Date().getFullYear()}
             {'.'}
+            <Link color="inherit" href="https://github.com/negin-shahani/Tic-Tac-Toe-React">
+                You can find my code in github  <GitHubIcon fontSize="inherit" />
+            </Link>
         </Typography>
     );
 }
