bb61d5021e51d21535e9f82809ff0896d04610b0
Taher <taherfattahi11@gmail.com>
2022-07-12 13:21:18 +0430

    socket.on('join') function for joining users

--- server/server.js
+++ server/server.js
@@ -3,11 +3,36 @@ const io = require("socket.io")(http);
 
 const port = 3000;
 
+let users = []
+
 //Listen for a client connection 
 io.on("connection", (socket) => {
     //Socket is a Link to the Client 
     console.log("New Client is Connected!");
     
+    socket.on('join', function (roomName, userName) {
+
+        const admin = users.filter(user => (user.admin === true) && (user.room === roomName));
+
+        if (admin.length > 0) {
+
+        } else {
+            // Admin
+            users.push({
+                'socketid': socket.id,
+                'socket': socket,
+                'userName': userName,
+                'admin': true,
+                'roomName': roomName,
+            });
+
+            socket.join(room);
+
+            socket.emit('newJoin', socket.id);
+        }
+    });
+
+
 });
 
 //Listen the HTTP Server 
